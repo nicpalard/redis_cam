e20ffa86da970cab6b3c219e1c86bd2f65ff95fc
Nicolas PALARD <nicolas.palard@etu.u-bordeaux.fr>
2018-05-30 15:44:21 +0200

    :racehorse: Fixed memory leak

--- example/src/client.cpp
+++ example/src/client.cpp
@@ -11,13 +11,15 @@ int main(int argc, char** argv)
     if (!client.connect()) { std::cerr << "Client could not connect to redis server" << std::endl; }
     client.setCameraKey("custom:image");
 
+    cv::Mat frame, displayFrame;
+    Image* cFrame;
     while (true)
     {
-        Image* cFrame = client.getImage();
+        cFrame = client.getImage();
         if (cFrame == NULL) { std::cerr << "Error: Could not get camera frame, exiting..." << std::endl; return EXIT_FAILURE;}
-        cv::Mat frame(cFrame->height(), cFrame->width(), CV_8UC3, (void*)cFrame->data());
-        cv::Mat displayFrame;
+        frame = cv::Mat(cFrame->height(), cFrame->width(), CV_8UC3, (void*)cFrame->data());
         cv::cvtColor(frame, displayFrame, CV_RGB2BGR);
+
         cv::imshow("frame", displayFrame);
         if (cv::waitKey(30) >= 0)
         {
@@ -25,4 +27,7 @@ int main(int argc, char** argv)
         }
         delete cFrame;
     }
+
+    frame.release();
+    displayFrame.release();
 }
